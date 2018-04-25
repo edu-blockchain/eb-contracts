e90d333a688df971301bae708f23ee7bfb099217
Chuck LeDuc Díaz <celeduc@gmail.com>
2018-04-25 13:54:17 +0200

    initial version

---
+++ README.md
@@ -0,0 +1,4 @@
+[![Build Status](https://travis-ci.org/edu-blockchain/eb-contracts.svg?branch=master)](https://travis-ci.org/edu-blockchain/eb-contracts)
+[![Coverage Status](https://coveralls.io/repos/github/edu-blockchain/eb-contracts/badge.svg?branch=master)](https://coveralls.io/github/edu-blockchain/eb-contracts?branch=master)
+
+# eb-contracts
---
+++ contracts/Recorder.sol
@@ -0,0 +1,32 @@
+pragma solidity ^0.4.21;
+
+import "./Ownable.sol";
+
+
+contract Recorder is Ownable {
+
+//  event BalanceDeposited(address from, uint value);
+
+//  struct AlertType {
+//    bytes name;
+//    bool active;
+//    uint price;
+//  }
+
+//  modifier onlyValidAlertType(uint id) {
+//    require(id < alertTypes.length);
+//    _;
+//  }
+
+//  AlertType[] public alertTypes;
+
+//  mapping(address => Subscriber) internal subscribers;
+
+//  function Alerter() public {
+//  }
+
+//  function () public payable {
+//    receiveFunds();
+//  }
+
+}
